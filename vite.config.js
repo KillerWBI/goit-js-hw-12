7c73e6874e7c9629f27cb33df052870416ea9d61
GetUp <108660418+KillerWBI@users.noreply.github.com>
2025-07-01 23:52:38 +0200

    цу

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
-  base: '/goit-js-hw-12/',
+  base: '/goit-js-hw-12/', // обязательно с именем репозитория
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
 });
